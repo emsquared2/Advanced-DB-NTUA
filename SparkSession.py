86dfe738a74b9ef2e9b4ec164a3c843efa6e8b60
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2023-12-23 21:52:00 +0200

    Modified SparkSession for RDDs

--- SparkSession.py
+++ SparkSession.py
@@ -2,3 +2,10 @@ from pyspark.sql import SparkSession
 
 def create_spark_session(app_name):
     return SparkSession.builder.appName(app_name).getOrCreate()
+
+def create_spark_session_rdd(app_name):
+    return SparkSession \
+        .builder \
+        .appName(app_name) \
+        .getOrCreate() \
+        .sparkContext
