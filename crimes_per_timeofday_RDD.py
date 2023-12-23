45fc7fc60c5ed81eedc88e9908d6578d9de65dc9
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2023-12-23 21:54:41 +0200

    RDD implementation to Rank Day Segments based on Crime Count

---
+++ crimes_per_timeofday_RDD.py
@@ -0,0 +1,34 @@
+from SparkSession import create_spark_session_rdd
+from import_data import import_crime_data_rdd
+
+sc = create_spark_session_rdd("Crime Rates Across Daytime Segments - RDD API")
+
+crime_rdd = import_crime_data_rdd(sc)
+
+# Function that identifies the day segment of a certain time
+def daytime_segment(time):
+    try:
+        hours = int(time[0:2])
+    except ValueError:
+        return 'UNKNOWN'
+    if 5 <= hours < 12:
+        return 'MORNING'
+    elif 12 <= hours < 16:
+        return 'AFTERNOON'
+    elif 16 <= hours < 21:
+        return 'EVENING'
+    else:
+        return 'NIGHT'
+
+
+crime_rdd = crime_rdd.filter(lambda line: line[15] == 'STREET') \
+                     .map(lambda line: (daytime_segment(line[3]), 1)) \
+                     .reduceByKey(lambda x, y: x + y) \
+                     .sortBy(lambda x: x[1], ascending=False)
+
+print(crime_rdd.collect())
+
+# Uncomment if you want the output saved in a text file
+# crime_rdd.saveAsTextFile("./query2-RDD.txt")
+
+sc.stop()
